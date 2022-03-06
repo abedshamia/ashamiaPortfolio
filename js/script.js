6bbc6b7188b1c24f2b9eee9f5640f31635ab5cbb
abedshamia <ab.shamia96@gmail.com>
2022-03-06 05:31:50 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav');
+const heroSection = document.querySelector('#Home');
+
+//after home scroll give nav background color
+window.addEventListener('scroll', () => {
+  console.log('scrolling');
+  if (window.scrollY > heroSection.clientHeight) {
+    nav.classList.add('nav-bg');
+  } else {
+    nav.classList.remove('nav-bg');
+  }
+});
