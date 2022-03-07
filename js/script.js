7950ad108f5bd3d9cb37ecf0a7f20860fcc8d81e
abedshamia <ab.shamia96@gmail.com>
2022-03-07 10:47:23 +0200

    Added content and animation

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ const heroSection = document.querySelector('#Home');
 
 //after home scroll give nav background color
 window.addEventListener('scroll', () => {
-  console.log('scrolling');
   if (window.scrollY > heroSection.clientHeight) {
     nav.classList.add('nav-bg');
   } else {
     nav.classList.remove('nav-bg');
   }
 });
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  const revealSections = document.querySelectorAll('.reveal');
+  const sectionsArray = Array.from(revealSections);
+
+  sectionsArray.forEach(section => {
+    const revealPoint = 150;
+    const windowHeight = window.innerHeight;
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight - revealPoint) {
+      section.classList.add('active');
+    }
+  });
+}
